e9d34a75aa4e9fac44e3891565c7e3da532f6a23
Your Name <tjd9248@naver.com>
2023-05-24 16:15:43 +0900

    Feat: DI Apply DI Apply

--- AppStoreClone/AppDelegate.swift
+++ AppStoreClone/AppDelegate.swift
@@ -15,7 +15,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        utilRegisterDependencies()
+        serviceRegisterDependencies()
+        
         return true
+    }
+    
+    private func utilRegisterDependencies() {
+        DIContainer.shared.register(GlobalAlert())
+        DIContainer.shared.register(ImageCache())
+        DIContainer.shared.register(ReadPList())
+        DIContainer.shared.register(ReadJson())
+        let globalAlert: GlobalAlert = DIContainer.shared.resolve()
+        let imageCache: ImageCache = DIContainer.shared.resolve()
+        let readPList: ReadPList = DIContainer.shared.resolve()
+        let readJson: ReadJson = DIContainer.shared.resolve()
+        DIContainer.shared.register(globalAlert)
+        DIContainer.shared.register(imageCache)
+        DIContainer.shared.register(readPList)
+        DIContainer.shared.register(readJson)
+    }
+    
+    private func serviceRegisterDependencies() {
+        DIContainer.shared.register(HeaderCommon())
+        DIContainer.shared.register(APIRequestService())
+        DIContainer.shared.register(URLService())
+        DIContainer.shared.register(ImageLoader())
+        DIContainer.shared.register(ItunesService())
+        let headerCommon: HeaderCommon = DIContainer.shared.resolve()
+        let apiRequestService: APIRequestService = DIContainer.shared.resolve()
+        let urlService: URLService = DIContainer.shared.resolve()
+        let imageLoader: ImageLoader = DIContainer.shared.resolve()
+        let itunesService: ItunesService = DIContainer.shared.resolve()
+        DIContainer.shared.register(headerCommon)
+        DIContainer.shared.register(apiRequestService)
+        DIContainer.shared.register(urlService)
+        DIContainer.shared.register(imageLoader)
+        DIContainer.shared.register(itunesService)
     }
 
     // MARK: UISceneSession Lifecycle
@@ -76,6 +113,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
 }
-
