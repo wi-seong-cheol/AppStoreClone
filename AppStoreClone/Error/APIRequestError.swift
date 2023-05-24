5f0565ce23fe8ea3a6a96e838ba28c05a884117e
Your Name <tjd9248@naver.com>
2023-05-24 16:03:43 +0900

    Feat: APIRequest Error Add APIRequest Error Add

---
+++ AppStoreClone/Error/APIRequestError.swift
@@ -0,0 +1,13 @@
+//
+//  APIRequestError.swift
+//  AppStoreClone
+//
+//  Created by wi_seong on 2023/05/23.
+//
+
+import Foundation
+
+enum APIRequestError: Error {
+    case invalidData
+    case invalidURL
+}
