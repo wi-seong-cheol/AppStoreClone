19764021f9f911d6e663750916fa8df6e0bfd162
Your Name <tjd9248@naver.com>
2023-05-24 16:09:40 +0900

    Feat: GridCollectionViewFlowLayout Add GridCollectionViewFlowLayout Add

---
+++ AppStoreClone/Util/GridCollectionViewFlowLayout.swift
@@ -0,0 +1,31 @@
+//
+//  GridCollectionViewFlowLayout.swift
+//  BoilerPlate
+//
+//  Created by wi_seong on 2023/05/18.
+//
+
+import Foundation
+import UIKit
+
+class GridCollectionViewFlowLayout: UICollectionViewFlowLayout {
+    
+    var ratioHeightToWidth = 1.0
+    var numberOfColumns = 1
+    
+    var cellSpacing = 0.0 {
+        didSet {
+            self.minimumLineSpacing = self.cellSpacing
+            self.minimumInteritemSpacing = self.cellSpacing
+        }
+    }
+    
+    override init() {
+        super.init()
+        self.scrollDirection = .vertical
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
