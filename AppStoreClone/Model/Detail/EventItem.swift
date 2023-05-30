0d6b869ff5eb6c9e4ee96ba3b3188f754655f7dc
Your Name <tjd9248@naver.com>
2023-05-30 10:35:30 +0900

    Feat: EventItem Add EventItem Add

---
+++ AppStoreClone/Model/Detail/EventItem.swift
@@ -0,0 +1,13 @@
+//
+//  EventItem.swift
+//  AppStoreClone
+//
+//  Created by wi_seong on 2023/05/27.
+//
+
+import Foundation
+
+struct EventItem {
+    var time: String?
+    var thumbnail: String?
+}
