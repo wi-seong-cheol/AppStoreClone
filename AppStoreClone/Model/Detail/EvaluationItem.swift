e7167b1e1b8b4bfa0430e6047602313da02c3686
Your Name <tjd9248@naver.com>
2023-05-30 10:35:53 +0900

    Feat: EvaluationItem Add EvaluationItem Add

---
+++ AppStoreClone/Model/Detail/EvaluationItem.swift
@@ -0,0 +1,18 @@
+//
+//  EvaluationItem.swift
+//  AppStoreClone
+//
+//  Created by wi_seong on 2023/05/27.
+//
+
+import Foundation
+
+struct EvaluationItem {
+    var grade: Double?
+    var five: Double?
+    var four: Double?
+    var three: Double?
+    var two: Double?
+    var one: Double?
+    var total: Int?
+}
