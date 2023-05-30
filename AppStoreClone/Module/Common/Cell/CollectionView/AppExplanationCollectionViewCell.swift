43d518f14561bbe9d11d51f034e4d6ead8e5fce0
Your Name <tjd9248@naver.com>
2023-05-30 10:49:11 +0900

    Feat: AppExplanationCollectionViewCell Add AppExplanationCollectionViewCell Add

---
+++ AppStoreClone/Module/Common/Cell/CollectionView/AppExplanationCollectionViewCell.swift
@@ -0,0 +1,83 @@
+//
+//  AppExplanationCollectionViewCell.swift
+//  AppStoreClone
+//
+//  Created by wi_seong on 2023/05/27.
+//
+
+import UIKit
+
+import RxSwift
+
+final class AppExplanationCollectionViewCell: UICollectionViewCell {
+    
+    var disposeBag = DisposeBag()
+    let onData: AnyObserver<ExplanationItem>
+    
+    private let firstContent: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let secondContent: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let more: UILabel = {
+        let label = UILabel()
+        label.text = "더 보기"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textAlignment = .right
+        label.textColor = UIColor.systemBlue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        let data = PublishSubject<ExplanationItem>()
+        onData = data.asObserver()
+        super.init(frame: frame)
+        setup(data: data)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setup(data: PublishSubject<ExplanationItem>) {
+        data.observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] item in
+                guard let self = self else { return }
+                firstContent.text = item.description
+                secondContent.text = "item.descrfsdsdfsafasfso÷n"
+            })
+            .disposed(by: disposeBag)
+        
+        contentView.addSubview(firstContent)
+        NSLayoutConstraint.activate([
+            firstContent.topAnchor.constraint(equalTo: topAnchor),
+            firstContent.leadingAnchor.constraint(equalTo: leadingAnchor),
+            firstContent.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+        
+        contentView.addSubview(secondContent)
+        secondContent.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        NSLayoutConstraint.activate([
+            secondContent.topAnchor.constraint(equalTo: firstContent.bottomAnchor),
+            secondContent.leadingAnchor.constraint(equalTo: leadingAnchor)
+        ])
+        
+        contentView.addSubview(more)
+        NSLayoutConstraint.activate([
+            more.centerYAnchor.constraint(equalTo: secondContent.centerYAnchor),
+            more.leadingAnchor.constraint(equalTo: secondContent.trailingAnchor, constant: 4),
+            more.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+}
