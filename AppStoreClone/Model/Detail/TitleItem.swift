778e9a5eabc6ec6fc907f45a5df69be8112e3bcc
Your Name <tjd9248@naver.com>
2023-05-30 10:32:19 +0900

    Feat: TitleItem Add TitleItem Add

---
+++ AppStoreClone/Model/Detail/TitleItem.swift
@@ -0,0 +1,15 @@
+//
+//  TitleItem.swift
+//  AppStoreClone
+//
+//  Created by wi_seong on 2023/05/27.
+//
+
+import Foundation
+
+struct TitleItem {
+    var thumbNail: String?
+    var title: String?
+    var subTitle: String?
+    var isDownload: Bool?
+}
