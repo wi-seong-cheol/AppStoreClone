4dd28ed969f854af9f1f3b436c9d67b7e3093e1a
Your Name <tjd9248@naver.com>
2023-05-30 10:36:06 +0900

    Feat: ReviewItem Add ReviewItem Add

---
+++ AppStoreClone/Model/Detail/ReviewItem.swift
@@ -0,0 +1,16 @@
+//
+//  ReviewItem.swift
+//  AppStoreClone
+//
+//  Created by wi_seong on 2023/05/27.
+//
+
+import Foundation
+
+struct ReviewItem {
+    var title: String?
+    var date: String?
+    var nickname: String?
+    var review: String?
+    var grade: Int?
+}
