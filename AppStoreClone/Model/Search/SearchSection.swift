996b567fd9d40a401a7eaaa0ef28ca17aa42e6d0
Your Name <tjd9248@naver.com>
2023-05-30 13:29:29 +0900

    Feat: Add SearchSection Add SearchSection

---
+++ AppStoreClone/Model/Search/SearchSection.swift
@@ -0,0 +1,18 @@
+//
+//  SearchSection.swift
+//  AppStoreClone
+//
+//  Created by wi_seong on 2023/05/30.
+//
+
+import Foundation
+
+struct SearchSection {
+    let type: SearchItemType
+    var items: [Any]
+}
+
+enum SearchItemType {
+    case new
+    case suggestion
+}
