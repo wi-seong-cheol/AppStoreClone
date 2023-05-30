d5931473e7d5ff6882b4daf69b88cb7a53228ad2
Your Name <tjd9248@naver.com>
2023-05-30 10:45:40 +0900

    Feat: ImageHeaderView Add ImageHeaderView Add

---
+++ AppStoreClone/Module/Common/View/ImageHeaderView.swift
@@ -0,0 +1,61 @@
+//
+//  CollectionReusableView.swift
+//  AppStoreClone
+//
+//  Created by wi_seong on 2023/05/27.
+//
+
+import UIKit
+
+import RxSwift
+
+final class ImageHeaderView: UICollectionReusableView {
+
+    private lazy var imageView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleToFill
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let cellDisposeBag = DisposeBag()
+    private var disposeBag = DisposeBag()
+    
+    let onData: AnyObserver<Void>
+    
+    override init(frame: CGRect) {
+        let data = PublishSubject<Void>()
+        onData = data.asObserver()
+        super.init(frame: frame)
+        setup(data: data)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup(data: PublishSubject<Void>) {
+        backgroundColor = .gray
+        
+        ImageLoader.cache_loadImage(url: "https://images.applypixels.com/images/originals/1696b13e-7eb7-4fd0-83a1-bb89d5aa5ab8.png")
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] image in
+                guard let self = self else { return }
+                self.imageView.image = image
+            })
+            .disposed(by: disposeBag)
+        
+        addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
+    }
+}
