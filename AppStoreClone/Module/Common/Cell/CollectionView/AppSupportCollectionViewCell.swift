961938ffd8ce131340acce9dc331ac168abe17c6
Your Name <tjd9248@naver.com>
2023-05-30 14:28:51 +0900

    Feat: Add AppSupportCollectionViewCell Add AppSupportCollectionViewCell

---
+++ AppStoreClone/Module/Common/Cell/CollectionView/AppSupportCollectionViewCell.swift
@@ -0,0 +1,82 @@
+//
+//  AppSupportCollectionViewCell.swift
+//  AppStoreClone
+//
+//  Created by wi_seong on 2023/05/27.
+//
+
+import UIKit
+
+import RxSwift
+
+final class AppSupportCollectionViewCell: UICollectionViewCell {
+    
+    var disposeBag = DisposeBag()
+    
+    let onData: AnyObserver<SupportItem>
+    
+    private let developerID: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let developer: UILabel = {
+        let label = UILabel()
+        label.text = "개발자"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = NSLayoutConstraint.Axis.vertical
+        stackView.distribution = UIStackView.Distribution.equalSpacing
+        stackView.alignment = UIStackView.Alignment.leading
+        stackView.spacing = 4.0
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    private let more: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "chevron.right")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        let data = PublishSubject<SupportItem>()
+        onData = data.asObserver()
+        super.init(frame: frame)
+        setup(data: data)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setup(data: PublishSubject<SupportItem>) {
+        data.observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] item in
+                guard let self = self else { return }
+            })
+            .disposed(by: disposeBag)
+        
+        stackView.addArrangedSubview(developerID)
+        stackView.addArrangedSubview(developer)
+        contentView.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor)
+        ])
+        
+        contentView.addSubview(more)
+        NSLayoutConstraint.activate([
+            more.centerYAnchor.constraint(equalTo: centerYAnchor),
+            more.leadingAnchor.constraint(greaterThanOrEqualTo: stackView.trailingAnchor, constant: 4),
+            more.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+}
