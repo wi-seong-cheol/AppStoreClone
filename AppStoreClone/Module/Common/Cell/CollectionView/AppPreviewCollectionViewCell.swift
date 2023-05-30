1a1a8c2ab3a353cd084a32b58f8fcb38f851fd43
Your Name <tjd9248@naver.com>
2023-05-30 10:48:54 +0900

    Feat: AppPreviewCollectionViewCell Add AppPreviewCollectionViewCell Add

---
+++ AppStoreClone/Module/Common/Cell/CollectionView/AppPreviewCollectionViewCell.swift
@@ -0,0 +1,70 @@
+//
+//  AppPreviewCollectionViewCell.swift
+//  AppStoreClone
+//
+//  Created by wi_seong on 2023/05/27.
+//
+
+import UIKit
+
+import RxSwift
+
+final class AppPreviewCollectionViewCell: UICollectionViewCell {
+    
+    var disposeBag = DisposeBag()
+    let onData: AnyObserver<PreviewItem>
+    
+    private let content: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 10
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        let data = PublishSubject<PreviewItem>()
+        onData = data.asObserver()
+        super.init(frame: frame)
+        setup(data: data)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setup(data: PublishSubject<PreviewItem>) {
+        data.observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] item in
+                guard let self = self else { return }
+                switch item.content {
+                case let .photo(data):
+                    ImageLoader.cache_loadImage(url: data.url)
+                        .observe(on: MainScheduler.instance)
+                        .subscribe(onNext: { [weak self] image in
+                            guard let self = self else { return }
+                            self.content.image = image
+                        })
+                        .disposed(by: disposeBag)
+                case let .video(video):
+                    // Preview 비디오 타입 코드 작성
+                    break
+                case .none:
+                    break
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        contentView.addSubview(content)
+        NSLayoutConstraint.activate([
+            content.topAnchor.constraint(equalTo: topAnchor),
+            content.bottomAnchor.constraint(equalTo: bottomAnchor),
+            content.leadingAnchor.constraint(equalTo: leadingAnchor),
+            content.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+}
