f658e7e2417d29d5f343825da27b076d6d16be88
Your Name <tjd9248@naver.com>
2023-05-30 13:30:30 +0900

    Feat: Add SuggestionItem Add SuggestionItem

---
+++ AppStoreClone/Model/Search/SuggestionItem.swift
@@ -0,0 +1,14 @@
+//
+//  SuggestionItem.swift
+//  AppStoreClone
+//
+//  Created by wi_seong on 2023/05/30.
+//
+
+import Foundation
+
+struct SuggestionItem {
+    var appIcon: String?
+    var title: String?
+    var desc: String?
+}
