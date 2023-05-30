59f5c99891da3fa4f83d7d97bebd436194d7f591
Your Name <tjd9248@naver.com>
2023-05-30 10:36:28 +0900

    Feat: FeatureItem Add FeatureItem Add

---
+++ AppStoreClone/Model/Detail/FeatureItem.swift
@@ -0,0 +1,14 @@
+//
+//  FeatureItem.swift
+//  AppStoreClone
+//
+//  Created by wi_seong on 2023/05/27.
+//
+
+import Foundation
+
+struct FeatureItem {
+    var version: String?
+    var date: String?
+    var content: String?
+}
