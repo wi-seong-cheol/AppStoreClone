a3b45396692bd90cb4ce546d52e2559f71c9a88a
Your Name <tjd9248@naver.com>
2023-06-06 21:50:54 +0900

    Feat: Data Binding Data Binding

--- AppStoreClone/Module/Search/SearchViewModel.swift
+++ AppStoreClone/Module/Search/SearchViewModel.swift
@@ -5,13 +5,17 @@
 //  Created by wi_seong on 2023/05/23.
 //
 
+import Foundation
+
 import RxCocoa
 import RxSwift
 
-typealias SearchViewModelInput = (firstInput: AnyObserver<Void>,
-                                  secondInput: AnyObserver<Void>)
+typealias SearchViewModelInput = (fetch: AnyObserver<Void>,
+                                  searchText: AnyObserver<String>,
+                                  error: AnyObserver<Error>)
 typealias SearchViewModelOutput = (datasource: Driver<[SearchSection]>,
-                                   secondOutput: Observable<Void>)
+                                   appData: Driver<[Result]>,
+                                   errorMessage: Observable<String>)
 
 protocol SearchViewModelType {
     var input: SearchViewModelInput { get }
@@ -25,45 +29,76 @@ final class SearchViewModel: SearchViewModelType {
     private let disposeBag = DisposeBag()
     
     // MARK: INPUT
-    private let firstInput = PublishSubject<Void>()
-    private let secondInput = PublishSubject<Void>()
+    private let fetch = PublishSubject<Void>()
+    private let searchText = PublishSubject<String>()
+    private let error = PublishSubject<Error>()
     
     var input: SearchViewModelInput {
-        return (firstInput.asObserver(),
-                secondInput.asObserver())
+        return (fetch.asObserver(),
+                searchText.asObserver(),
+                error.asObserver())
     }
     
     // MARK: OUTPUT
-    private let datasource = BehaviorRelay<[SearchSection]>(value: [
-        SearchSection(type: .new, items: [
-            NewItem(title: "배달"),
-            NewItem(title: "fifa"),
-            NewItem(title: "씽씽"),
-            NewItem(title: "아이돌 키우기"),
-        ]),
-        
-        SearchSection(type: .suggestion, items: [
-            SuggestionItem(appIcon:  "https://images.applypixels.com/images/originals/1696b13e-7eb7-4fd0-83a1-bb89d5aa5ab8.png",
-                           title: "abcdlsjfnldanslfnldasn",
-                           desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."),
-            SuggestionItem(appIcon:  "https://images.applypixels.com/images/originals/1696b13e-7eb7-4fd0-83a1-bb89d5aa5ab8.png",
-                           title: "abcsdafsafsafsaf",
-                           desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."),
-            SuggestionItem(appIcon:  "https://images.applypixels.com/images/originals/1696b13e-7eb7-4fd0-83a1-bb89d5aa5ab8.png",
-                           title: "abc",
-                           desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."),
-            SuggestionItem(appIcon:  "https://images.applypixels.com/images/originals/1696b13e-7eb7-4fd0-83a1-bb89d5aa5ab8.png",
-                           title: "abc",
-                           desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."),
-        ])
-    ])
-    private let secondOutput = PublishSubject<Void>()
+    private let datasource = BehaviorRelay<[SearchSection]>(value: [])
+    private let appData = BehaviorRelay<[Result]>(value: [])
+    private let errorMessage = PublishSubject<String>()
     
     var output: SearchViewModelOutput {
         return (datasource: datasource.asDriver(onErrorJustReturn: []),
-                secondOutput: secondOutput.asObservable())
+                appData: appData.asDriver(onErrorJustReturn: []),
+                errorMessage: errorMessage.asObserver())
     }
     
     init() {
+        let newDiscovery = service.newDiscovery()
+        let recommendApp = service.recommendApp()
+        let zipped = Observable.zip(newDiscovery, recommendApp)
+        
+        fetch
+            .flatMap { zipped.materialize() }
+            .subscribe(onNext: { [weak self] materializedEvent in
+                switch materializedEvent {
+                case let .next((newDiscoveryResponse, recommendAppResponse)):
+                    guard let newDiscoveryResults = newDiscoveryResponse.results,
+                          let recommendAppResults = recommendAppResponse.results else {
+                        break
+                    }
+                    let newItem = newDiscoveryResults.map { NewItem(title: $0.trackName) }
+                    let recommendItem = recommendAppResults.map {
+                        RecommendItem(appIcon: $0.artworkUrl512,
+                                      title: $0.trackName,
+                                      desc: $0.description)
+                    }
+                    self?.appData.accept(recommendAppResults)
+                    self?.datasource.accept([
+                        SearchSection(type: .new, items: newItem),
+                        SearchSection(type: .recommend, items: recommendItem)
+                    ])
+                case let .error(error):
+                    self?.error.onNext(error)
+                case .completed: break
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        error
+            .subscribe(onNext: { [weak self] error in
+                guard let self = self else { return }
+                guard let error = error as? APIRequestError else {
+                    self.errorMessage.onNext(error.localizedDescription)
+                    return
+                }
+                
+                switch error {
+                case .invalidData:
+                    self.errorMessage.onNext("잘못된 요청입니다. 다시 시도해 주세요.")
+                case .invalidURL:
+                    self.errorMessage.onNext("잘못된 URL입니다. 다시 시도해 주세요.")
+                case .isNotConnect:
+                    break
+                }
+            })
+            .disposed(by: disposeBag)
     }
 }
