55e7f5f142b4a7621b01b4165548ae6825e31b84
Your Name <tjd9248@naver.com>
2023-06-06 21:08:08 +0900

    Feat: Add Service Function Add Service Function

--- AppStoreClone/Service/API/ItunesService.swift
+++ AppStoreClone/Service/API/ItunesService.swift
@@ -10,6 +10,8 @@ import Foundation
 import RxSwift
 
 protocol ItunesFetchable {
+    func newDiscovery() -> Observable<ItunesResponse>
+    func recommendApp() -> Observable<ItunesResponse>
     func search(_ term: String) -> Observable<ItunesResponse>
 }
 
@@ -18,15 +20,44 @@ class ItunesService: ItunesFetchable {
     @Inject private var urlService: URLService
     @Inject private var apiRequestService: APIRequestService
     
-    func search(_ term: String) -> Observable<ItunesResponse> {
+    func newDiscovery() -> Observable<ItunesResponse> {
         let url: String = urlService.serviceUrl(.sample)
         let query: [String: Any] = [
-            "media": "music",
-            "entity": "song",
-            "term": term
+            "country": "kr",
+            "entity": "software",
+            "term": "새로운 발견",
+            "limit": 4,
+            "sort": "recent"
         ]
         let interceptor: Interceptor = BaseInterceptor()
         
-        return apiRequestService.getable(url: url, query: query, interceptor: interceptor) ?? Observable.empty()
+        return apiRequestService.getable(url: url, query: query, interceptor: interceptor)
+    }
+    
+    func recommendApp() -> Observable<ItunesResponse> {
+        let url: String = urlService.serviceUrl(.sample)
+        let query: [String: Any] = [
+            "country": "kr",
+            "entity": "software",
+            "term": "인기 어플",
+            "limit": 16,
+            "sort": "recent"
+        ]
+        let interceptor: Interceptor = BaseInterceptor()
+        
+        return apiRequestService.getable(url: url, query: query, interceptor: interceptor)
+    }
+    
+    func search(_ term: String) -> Observable<ItunesResponse> {
+        let url: String = urlService.serviceUrl(.sample)
+        let query: [String: Any] = [
+            "country": "kr",
+            "entity": "software",
+            "term": term,
+            "limit": 14,
+        ]
+        let interceptor: Interceptor = BaseInterceptor()
+        
+        return apiRequestService.getable(url: url, query: query, interceptor: interceptor)
     }
 }
